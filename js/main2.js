05ddee5034829ce4551b5b469ea92830cbdf9284
Miguelluisdev <126804912+Miguelluisdev@users.noreply.github.com>
2023-05-09 15:14:42 -0300

    b

---
+++ js/main2.js
@@ -0,0 +1,12 @@
+ function alerta(){
+  window.alert("Ctrl + e Ctrl - para aumentar e diminuir a fonte")
+}
+function menuClick(){
+  let nav = document.querySelector('.navbar');
+
+  if (nav.classList.contains("open")){
+    nav.classList.remove("open");
+  }else{
+    nav.classList.add("open");
+  }
+}
